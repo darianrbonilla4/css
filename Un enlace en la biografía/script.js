34da119d73b62d48c316d1ff094c488a5df2f459
Darian Bonilla <darianrbonilla@gmail.com>
2025-06-25 10:33:09 -0400

    Add files via upload

---
+++ Un enlace en la biografía/script.js
@@ -0,0 +1,14 @@
+function latestwork() {
+  window.open("https://jobmarketdo.netlify.app/");
+}
+function projects() {
+  window.open("https://darianbonilla.gumroad.com/");
+}
+function casestudy() {
+  window.open("https://github.com/darianrbonilla4?tab=repositories");
+}
+function projectform() {
+  window.open(
+    "https://docs.google.com/forms/d/e/1FAIpQLSfrgVY4bnhfzk2gdiwc90fTVUnmBzw-MYb0cp-r1xf2veBcGQ/viewform?usp=header"
+  );
+}
